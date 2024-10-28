d111ad7557cf3580167a3058a44acbf080976909
Rishabh Madan <rishabhmadan96@gmail.com>
2024-10-28 10:59:27 -0400

    minor changes

--- src/test_ros_node.py
+++ src/test_ros_node.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
 import unittest
+
 import rospy
 import rostest
 from std_msgs.msg import Bool
 
+
 class TestROSNode(unittest.TestCase):
 
     def setUp(self):
-        rospy.init_node('test_ros', anonymous=True)
+        rospy.init_node("test_ros", anonymous=True)
         self.received_message = None
 
     def test_subscriber_receives_messages(self):
-        self.received_message = rospy.wait_for_message('dog_status', Bool, timeout=10)
+        self.received_message = rospy.wait_for_message("dog_status", Bool, timeout=10)
 
-        print ("Received message from dog_status topic")
+        print("Received message from dog_status topic")
         self.assertEqual(self.received_message.data, True)
 
-if __name__ == '__main__':
-    rostest.rosrun('python_starter', 'test_ros_node', TestROSNode)
+
+if __name__ == "__main__":
+    rostest.rosrun("python_starter", "test_ros_node", TestROSNode)
